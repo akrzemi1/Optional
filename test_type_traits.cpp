35213087ca23aca4f1b1228bcb85610355bb7e9c
Andrzej Krzemienski <akrzemi1@gmail.com>
2012-11-29 21:08:36 +0100

    added traits test

---
+++ test_type_traits.cpp
@@ -0,0 +1,49 @@
+// Copyright (C) 2011 - 2012 Andrzej Krzemienski.
+//
+// Use, modification, and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+# include "optional.hpp"
+
+struct Val
+{
+  Val(){}
+  Val( Val const & ){}
+  Val( Val && ) noexcept {}
+  
+  Val & operator=( Val const & ) = delete;
+  Val & operator=( Val && ) noexcept = delete;
+};
+
+struct Safe
+{
+    Safe(){}
+    Safe( Safe const & ){}
+    Safe( Safe && ) noexcept {}
+    
+    Safe & operator=( Safe const & ){}
+    Safe & operator=( Safe && ) noexcept {}
+};
+
+struct Unsafe
+{
+    Unsafe(){}
+    Unsafe( Unsafe const & ){}
+    Unsafe( Unsafe && ){}
+    
+    Unsafe & operator=( Unsafe const & ){}
+    Unsafe & operator=( Unsafe && ) {}
+};
+
+
+static_assert(std::is_nothrow_move_constructible<Safe>::value, "WTF!");
+static_assert(!std::is_nothrow_move_constructible<Unsafe>::value, "WTF!");
+
+static_assert(std::is_assignable<Safe&, Safe&&>::value, "WTF!");
+static_assert(!std::is_assignable<Val&, Val&&>::value, "WTF!");
+
+static_assert(std::is_nothrow_move_assignable<Safe>::value, "WTF!");
+static_assert(!std::is_nothrow_move_assignable<Unsafe>::value, "WTF!");
+
+int main() { }
